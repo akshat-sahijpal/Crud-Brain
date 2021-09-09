c673ede2b4825c3b49ea6885e9e4038b67e970ec
akshat <20akshatsahijpal01droid01@gmail.com>
2021-09-09 07:44:23 +0530

    version 1.0

---
+++ src/main/kotlin/com/akshatsahijpal/Crud/Brain/CrudBrainApplication.kt
@@ -0,0 +1,11 @@
+package com.akshatsahijpal.Crud.Brain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudBrainApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudBrainApplication>(*args)
+}
---
+++ src/test/kotlin/com/akshatsahijpal/Crud/Brain/CrudBrainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.akshatsahijpal.Crud.Brain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudBrainApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
